b95441748d64eec73c181e81de88e5e55fb00206
rojamich <rojamich@oregonstate.edu>
2024-11-05 19:10:27 -0800

    package node and express plus basic handlebars template

---
+++ server.js
@@ -0,0 +1,35 @@
+var fs = require("fs")
+var express = require('express')
+var exphbs = require('express-handlebars')
+
+var app = express()
+var port = process.env.PORT || 3000
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(function (req, res, next) {
+    console.log("== New Request")
+    console.log(" -- URL:", req.url)
+    console.log(" -- Method:", req.method)
+
+    console.log("----------------------------------------------------")
+    next()
+})
+
+app.use(express.json())
+app.use(express.static('static'))
+
+app.get('/', function (req, res, next) {
+  res.status(200).render('homePage')
+})
+
+app.get('*', function (req, res, next) {
+    res.status(404).render('404', {
+      page: req.url
+    })
+  })
+
+app.listen(port, function () {
+    console.log("== Server listening on port", port)
+  })
